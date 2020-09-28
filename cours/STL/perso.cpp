1acef4d53d29f1a7a91e020945a6a8ab4f0e1864
HenriBrg <hnr.berger@gmail.com>
2020-09-28 21:49:25 +0200

    Add resources

---
+++ cours/STL/perso.cpp
@@ -0,0 +1,91 @@
+  /*
+  
+  
+  ft::List<int> mylist;
+  for (int i=1; i<=5; ++i) mylist.push_back(i);
+  std::cout << "mylist backwards:";
+  for (ft::List<int>::reverse_iterator rit=mylist.rbegin(); rit!=mylist.rend(); ++rit)
+    std::cout << ' ' << *rit;
+  std::cout << '\n';
+
+    std::list<int> mylist2;
+  for (int i=1; i<=5; ++i) mylist2.push_back(i);
+  std::cout << "mylist backwards:";
+  for (std::list<int>::reverse_iterator rit=mylist2.rbegin(); rit!=mylist2.rend(); ++rit)
+    std::cout << ' ' << *rit;
+  std::cout << '\n';
+
+  // ---------------
+
+  // std::list<int> test;
+  // test.push_back(10);
+  // test.push_back(20);
+  // test.push_back(30);
+  // std::list<int>::iterator it = test.begin();
+  // it--;
+  // it--;
+  // std::cout << *it << std::endl;
+  // std::list<int>::iterator it2 = test.end();
+  // it2++;
+  // std::cout << *it2 << std::endl;
+
+  // // ----------------
+
+  // ft::List<int> test2;
+  // test2.push_back(10);
+  // test2.push_back(20);
+  // test2.push_back(30);
+  // ft::List<int>::iterator xit = test2.begin();
+  // xit--;
+  // xit--;
+  // std::cout << *xit << std::endl;
+
+  //   ft::List<int>::iterator xit2 = test2.end();
+  // xit2++;
+  // std::cout << *xit2 << std::endl;
+
+  ft::List<int> mine(3, 1);
+  ft::List<int>::const_iterator itx = mine.begin();
+  std::cout << *itx << std::endl;
+
+*/
+
+/*
+
+
+  ft::Vector<int> myvec;
+  for (int i=1; i<=5; ++i) myvec.push_back(i);
+  std::cout << "myvec backwards:";
+  for (ft::Vector<int>::reverse_iterator rit=myvec.rbegin(); rit!=myvec.rend(); ++rit)
+    std::cout << ' ' << *rit;
+  std::cout << '\n';
+
+    std::vector<int> myvec2;
+  for (int i=1; i<=5; ++i) myvec2.push_back(i);
+  std::cout << "myvec backwards:";
+  for (std::vector<int>::reverse_iterator rit=myvec2.rbegin(); rit!=myvec2.rend(); ++rit)
+    std::cout << ' ' << *rit;
+  std::cout << '\n';
+
+
+//   ft::Vector<int> vecx(1, 42);
+
+//   ft::Vector<int>::const_iterator itx = vecx.begin();
+//   std::cout << *itx << std::endl;
+
+*/
+
+/*
+
+
+
+	// std::map<char, int> one;
+	// ft::Map<char, int> two;
+	// one['A'] = 100;
+	// two['A'] = 100;
+	// std::cout << one.max_size() << std::endl;
+	// std::cout << two.max_size() << std::endl;
+
+    // return (0);
+
+*/
